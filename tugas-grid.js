a3a5730eaf59b5693df8f56c9a6f6923d3270481
yulianto <jooliantoo@gmail.com>
2023-10-10 10:48:37 +0700

    second commit

--- tugas-grid.js
+++ tugas-grid.js
@@ -1,53 +1,12 @@
-class Grid {
-  constructor(init) {
-    this.init = init;
-  }
-
-  createHeader(data) {
-    let open = "<thead><tr>";
-    let close = "</tr></thead>";
-    data.forEach((d) => {
-      open += `<th>${d}</th>`;
-    });
-
-    return open + close;
-  }
-
-  createBody(data) {
-    let open = "<tbody>";
-    let close = "</tbody>";
-
-    data.forEach((d) => {
-      open += `
-        <tr>
-          <td>${d[0]}</td>
-          <td>${d[1]}</td>
-        </tr>
-      `;
-    });
-
-    return open + close;
-  }
-
-  render(element) {
-    let grid =
-      "<table class='table table-hover'>" +
-      this.createHeader(this.init.columns) +
-      this.createBody(this.init.data) +
-      "</table>";
-    element.innerHTML = grid;
-  }
-}
-
-const grid = new Grid({
-  columns: ["Name", "Email"],
-  data: [
-    ["Kwon Yuli", "kwonyuli@gmail.com"],
-    ["Yulianto", "yulianto@gmail.com"]
-  ]
-});
-const app = document.getElementById("app");
-grid.render(app);
-
-
-
+ 
+ 
+ new gridjs.Grid({
+      columns: ["Name", "Email", "Phone Number"],
+      data: [
+        ["John", "john@example.com", "(353) 01 222 3333"],
+        ["Mark", "mark@gmail.com", "(01) 22 888 4444"],
+        ["Eoin", "eoin@gmail.com", "0097 22 654 00033"],
+        ["Sarah", "sarahcdd@gmail.com", "+322 876 1233"],
+        ["Afshin", "afshin@mail.com", "(353) 22 87 8356"]
+      ]
+    }).render(document.getElementById("table-gridjs"));
